e875ae0557803d39df0465dfda1db4a2a46368db
louisks98 <louis.christophe.poulin@gmail.com>
2022-12-05 21:42:43 -0500

    init commit

---
+++ budget_app/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Budget App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.60),
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        onTap: (value) {
+          
+        },
+        items:const [
+          BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: 'Expenses',
+              icon: Icon(Icons.account_balance)),
+          BottomNavigationBarItem(
+              label: 'Budget',
+              icon: Icon(Icons.savings),)
+        ]
+      ),      
+    );
+  }
+
+}
